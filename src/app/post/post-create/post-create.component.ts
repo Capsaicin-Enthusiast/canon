42b337468b7b40c805380a7c954b93595895e180
Louis Jasper A. Cañon <132154593+Capsaicin-Enthusiast@users.noreply.github.com>
2025-01-28 18:00:06 +0800

    first commit

--- src/app/post/post-create/post-create.component.ts
+++ src/app/post/post-create/post-create.component.ts
@@ -1,14 +1,16 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-post-create',
   templateUrl: './post-create.component.html',
   styleUrl: './post-create.component.css',
-  imports: [FormsModule]
+  imports: [FormsModule, MatCardModule, MatFormFieldModule, MatInputModule]
 })
 export class PostCreateComponent {
-
   NewPost = '';
   PostInput = '';
 
