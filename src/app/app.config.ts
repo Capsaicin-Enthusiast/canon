4c623a7d788406d31d09301ed80fd69a295df89e
Louis Jasper A. Cañon <132154593+Capsaicin-Enthusiast@users.noreply.github.com>
2025-02-25 13:48:47 +0800

    feat: add support for fetch in HttpClient configuration and improve app provider structure

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,8 +5,14 @@ import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
-import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi, withFetch } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideHttpClient(withInterceptorsFromDi()), provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(withEventReplay()), provideAnimationsAsync()]
+  providers: [
+    provideHttpClient(withInterceptorsFromDi(), withFetch()),
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideClientHydration(withEventReplay()),
+    provideAnimationsAsync()
+  ]
 };
