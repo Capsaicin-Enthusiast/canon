e79cfc5ff44c9f73577b3053240007c8d67a39bc
Louis Jasper A. Cañon <132154593+Capsaicin-Enthusiast@users.noreply.github.com>
2025-02-28 16:20:02 +0800

    feat: implement CRUD operations for posts and add body-parser and CORS middleware

--- backend/app.js
+++ backend/app.js
@@ -1,30 +1,64 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 
 const app = express();
 
-app.use("/api/posts", (req, res, next) => {
-  const posts = [
-    {
-      id: "sdadadasda",
-      title: "1st server-side post",
-      content: "1st content from server-side",
-    },
-    {
-      id: "fgdfsfdfs",
-      title: "2nd server-side post",
-      content: "2nd content from server-side",
-    },
-    {
-      id: "dfsdfgfghg",
-      title: "3rd server-side post",
-      content: "3rd content from server-side",
-    },
-  ];
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(
+  cors({
+    origin: "http://localhost:4200",
+    methods: "GET,POST,PUT,DELETE",
+    allowedHeaders: "Content-Type,Authorization",
+  })
+);
 
+let posts = [
+  {
+    id: "sdadadasda",
+    title: "1st server-side post",
+    content: "1st content from server-side",
+  },
+  {
+    id: "fgdfsfdfs",
+    title: "2nd server-side post",
+    content: "2nd content from server-side",
+  },
+  {
+    id: "dfsdfgfghg",
+    title: "3rd server-side post",
+    content: "3rd content from server-side",
+  },
+];
+
+app.get("/api/posts", (req, res, next) => {
   res.status(200).json({
     message: "Posts fetched successfully!",
     posts: posts,
   });
+});
+
+app.post("/api/posts", (req, res, next) => {
+  const post = req.body;
+  console.log(post);
+  res.status(201).json({
+    message: "Post added successfully!",
+    postId: post.id,
+  });
+});
+
+app.put("/api/posts/:id", (req, res, next) => {
+  const updatedPost = req.body;
+  const postId = req.params.id;
+  posts = posts.map((post) => (post.id === postId ? updatedPost : post));
+  res.status(200).json({ message: "Post updated successfully!" });
+});
+
+app.delete("/api/posts/:id", (req, res, next) => {
+  const postId = req.params.id;
+  posts = posts.filter((post) => post.id !== postId);
+  res.status(200).json({ message: "Post deleted successfully!" });
 });
 
 module.exports = app;
